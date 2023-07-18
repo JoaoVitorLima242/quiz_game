5305c90f34e44599dadf0d32a0d78abcd67ac9be
JoaoVitorLima242 <jlima0500@gmail.com>
2023-07-18 17:04:33 -0300

    rendering content conditionally

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_game/questions_screen.dart';
 import 'package:quiz_game/start_screen.dart';
 
 const backgroundColor = Color(0xff54494b);
@@ -13,13 +14,21 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Container(
           color: backgroundColor,
-          child: const StartScreen(),
+          child: activeScreen,
         ),
       ),
     );
