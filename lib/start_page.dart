0fa7a066213de7cab3acb963a0dc864a07c2df3c
JoaoVitorLima242 <jlima0500@gmail.com>
2023-07-17 23:38:25 -0300

    starting home page

--- lib/start_page.dart
+++ lib/start_page.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
 
+const BackgroundColor = Color(0xff54494b);
+
 class StartPage extends StatelessWidget {
   const StartPage({super.key});
+
+  void onStartQuiz() {}
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.cyan,
-      child: const Center(
+      color: BackgroundColor,
+      child: Center(
         child: Column(
-          children: [Text('Starting')],
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'images/quiz-logo.png',
+              width: 300,
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 50, bottom: 30),
+              child: Text(
+                'Learn Flutter the fun way!',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 26,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: onStartQuiz,
+              child: const Text('Start Quiz'),
+            )
+          ],
         ),
       ),
     );
