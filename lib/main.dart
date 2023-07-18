245789a899c3cc5f447a7fee2e95ccf073d14259
JoaoVitorLima242 <jlima0500@gmail.com>
2023-07-17 23:06:31 -0300

    starting my app

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_game/start_page.dart';
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: StartPage(),
+    );
+  }
+}
